61baee37b949c29c1a482b9c6af363d3fea565d2
ltann <leeroyleeroy18@gmail.com>
2020-10-07 22:36:19 -0700

    Added ProductScreen and react router

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
 import products from '../products'
-import Product from './Pruduct'
+import Product from '../components/Pruduct'
 
 const HomeScreen = () => {
   return (
