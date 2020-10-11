669b7bf234e9a5cc53a97180d794782223449ff9
ltann <leeroyleeroy18@gmail.com>
2020-10-10 21:40:50 -0700

    Fetch product from DB and added errorhandling middleware

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+
+import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
 
@@ -10,17 +12,14 @@ connectDB()
 
 const app = express()
 
+app.use('/api/products', productRoutes)
+
+app.use(notFound)
+
+app.use(errorHandler)
+
 app.get('/', (req, res) => {
   res.send('API is running')
-})
-
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((product) => product._id === req.params.id)
-  res.json(product)
 })
 
 const PORT = process.env.PORT || 5000
