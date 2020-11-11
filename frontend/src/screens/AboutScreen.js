7ff3d39eec9338f3abff4d8290a24a58335a0a54
ltann <leeroyleeroy18@gmail.com>
2020-11-10 18:05:15 -0800

    Added AboutScreen section

---
+++ frontend/src/screens/AboutScreen.js
@@ -0,0 +1,63 @@
+import React from 'react'
+
+const AboutScreen = () => {
+  return (
+    <>
+      <h1>About us</h1>
+      <div>
+        <img
+          src='images/trolltunga2.jpg'
+          alt='about us pic'
+          className='aboutus-pic'
+        />
+      </div>
+
+      <p className='about-p'>
+        Beautiful Jewels opened in 1980 and built by offering great prices on
+        fine vintage jewelry, mint-condition pre-owned watches, loose diamonds,
+        and engagement rings, because that was what my customers wanted. As time
+        went on, I added select NEW designer jewelry lines because my customers
+        asked for them. Vintage and antique jewelry has always been a great
+        value for our customers, and the selection is incredible, including
+        diamond jewelry, platinum and gold jewelry, precious gemstones, colored
+        diamonds, and pearls. There is something for everyone and in every price
+        range. With new pieces arriving every day, you will find something
+        unique and precious every time you visit us. Like-new, pre-owned
+        wristwatches continue to be in strong demand, including such renowned
+        brands as Rolex, Cartier, Omega, IWC, TAG Heuer, and more. Fine Swiss
+        and German watches are built to last a lifetime so a reconditioned watch
+        not only looks and operates as good as new, it's a great value. Only you
+        will know you didn't pay full price! Even in these interesting times, we
+        want to celebrate the significant events in our lives with style; a
+        spectacular engagement ring or diamond anniversary band makes the moment
+        extraordinary. Our selection of new diamond jewelry has never been
+        better and we carry a vast array of engagement rings and diamond bands.
+        We also have a full-service, in-house custom shop to help you create
+        your own unique jewelry design or custom wedding bands. Offering
+        exceptional values on fine vintage jewelry, pre-owned watches, and new
+        engagement and wedding rings, coupled with our strong relationships with
+        brands such as Hearts On Fire®, Memoire®, Madison L, Beverly K, Venetti,
+        Hamilton, Wolf Designs, Denny Wong, and Tissot, will allow us to better
+        serve you in the value-driven future.
+      </p>
+      <p>
+        We take trade-ins of jewelry, gold, and watches, so bring in whatever
+        you don't want and trade it for what you do want! Trade your diamond for
+        a bigger one, perhaps even an incredible Hearts On Fire diamond! We have
+        also opened estate buying offices throughout San Diego County, so that
+        you don't have to drive as far to get cash for your valuables. We are
+        San Diego's largest buyer of pre-owned jewelry, scrap gold, coins,
+        sterling silverware, diamonds, and fine watches and we pay the highest
+        possible prices. Transactions are private, secure, and easy; we are your
+        trusted jeweler when it comes time to cash in that jewelry or watch that
+        you never wear. Your ongoing support has meant the world to us and we
+        want to give back to you through excellent service and great values.
+        Thank you for being a part of our family, and welcome aboard if you have
+        just joined us on the trip into a bright future with Beautiful Jewels.
+      </p>
+      <p>-- John Doe</p>
+    </>
+  )
+}
+
+export default AboutScreen
